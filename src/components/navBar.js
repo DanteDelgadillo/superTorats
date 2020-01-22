d0017ea536cdc3cb96a262548e5d3391b4b82729
dante <40077234+DanteDelgadillo@users.noreply.github.com>
2020-01-22 00:59:20 -0800

    update

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -5,18 +5,21 @@ export default class Navbar extends Component {
     render() {
         return (
             <React.Fragment>
-
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <a className="navbar-brand navbar" href="/">Navbar</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <a class="navbar-brand navbar" href="/">Navbar</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse " >
-                        <div className="navbar-nav ">
-                            <a className="nav-item nav-link " href="/">Home</a>
-                            <a className="nav-item nav-link" href="/Menu">Menu</a>
-                            <a className="nav-item nav-link" href="/">About</a>
-                        </div>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav navMargin ">
+                            <li class="nav-item ">
+                                <a class="nav-link" href="/">Home </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/Menu">Menu</a>
+                            </li>
+
+                        </ul>
                     </div>
                 </nav>
 
