ce74b6bd5b8956b98c7fe5126aadaa7527202b9e
dante <40077234+DanteDelgadillo@users.noreply.github.com>
2020-01-22 21:11:24 -0800

    update

--- src/components/home.js
+++ src/components/home.js
@@ -5,13 +5,37 @@ const Home = () => {
         <React.Fragment>
             <div className="home-flex-container">
                 <div className="sideimage">
-                    2222
+
                 </div>
                 <div className="homeFlex">
                     2222
                 </div>
                 <div className="sideimage2">
-                    2222
+
+                </div>
+            </div>
+            {/* ***********Our Location******* */}
+            <div className="location-container">
+                <div>
+                    <ul className="locat">
+                        <li className="locatTitle" >Our Location</li>
+                    </ul>
+                    <div className="loactText">
+                        <ul className="locat">
+                            <li>Super Tortas</li>
+                            <li>7949 Vineland Ave
+                            Sun Valley, CA 91352
+                            Sun Valley
+                            </li>
+                            <li>(818) 765-2496</li>
+                        </ul>
+                    </div>
+                    <div className="loactText">
+                        <ul className="locat">
+                            <li>Mon - Sat:10:00 am - 8:00 pm</li>
+                            <li>Sun: 11:00 am - 8:00 pm</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </React.Fragment>
